d82e038397847ba453b47b179226f8dee3d06c23
ArcadeFortune <alessionights@gmail.com>
2023-09-08 20:27:12 +0200

    feat: added every language

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,6 +1,7 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
+import React, { useState, useEffect } from "react";
+import { SelectLanguage } from "./SelectLanguage";
+import { SelectWords } from "./SelectWords";
+
 import Typography from "@mui/material/Typography";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
@@ -38,9 +39,24 @@ function getStepContent(step) {
 }
 
 export const MainContainer = () => {
-    
+  const [activeStep, setActiveStep] = useState(0);
+  const [lang, setLang] = useState("");
+  const [words, setWords] = useState("");
+  const [ok, setOk] = useState(false);
 
-  const [activeStep, setActiveStep] = React.useState(0);
+  const handleLangChange = (event) => {
+    setLang(event.target.value);
+  };
+
+  const handleWordsChange = (event) => {
+    setWords(event.target.value);
+  };
+
+  useEffect(() => {
+    if (lang !== "") {
+      setOk(true);
+    }
+  }, [lang]);
 
   const handleNext = () => {
     setActiveStep(activeStep + 1);
@@ -51,25 +67,40 @@ export const MainContainer = () => {
   };
 
   return (
-    
     <Container component="main" maxWidth="lg" sx={{ mb: 4 }}>
-        
       <CssBaseline />
+
       <Paper
         variant="outlined"
         sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
       >
-        <Typography component="h1" variant="h4" align="center">
-          Checkout
+        <Typography component="h1" variant="h4" align="center" sx={{ mb: 2 }}>
+          Quizlet
         </Typography>
-        <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-          {steps.map((label) => (
-            <Step key={label}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
+        <Divider />
+
+        <Typography
+          component="h1"
+          variant="h5"
+          align="left"
+          sx={{ pt: 3, pb: 1 }}
+        >
+          Chose a language
+        </Typography>
+
+        <SelectLanguage lang={lang} handleChange={handleLangChange} />
+        <Typography
+          component="h1"
+          variant="h5"
+          align="left"
+          sx={{ pt: 3, pb: 1 }}
+        >
+          Chose how many words will be blank
+        </Typography>
+        <SelectWords words={words} handleChange={handleWordsChange} />
+
         {activeStep === steps.length ? (
+          // victory screen
           <React.Fragment>
             <Typography variant="h5" gutterBottom>
               Thank you for your order.
@@ -90,11 +121,12 @@ export const MainContainer = () => {
                 </Button>
               )}
               <Button
+                disabled={!ok} // copilot OP
                 variant="contained"
                 onClick={handleNext}
                 sx={{ mt: 3, ml: 1 }}
               >
-                {activeStep === steps.length - 1 ? "Place order" : "Next"}
+                {ok ? "OK" : "OK"}
               </Button>
             </Box>
           </React.Fragment>
