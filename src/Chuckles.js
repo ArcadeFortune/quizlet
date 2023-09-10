afa7afb1734f91724ced676ee1a122fe3f4cc303
ArcadeFortune <alessionights@gmail.com>
2023-09-10 14:31:12 +0200

    feat: filtering api sentences for bad words

--- src/Chuckles.js
+++ src/Chuckles.js
@@ -1,23 +1,38 @@
 // function which returns a list with a specified amount of chuck norris jokes
 // using the chuck norris api
 // https://api.chucknorris.io/jokes/random
+import profanity from "./settings/BadWords.json";
 
-const fetchChuckNorrisJoke = async () => {
+const fetchChuckNorrisJoke = async (profanity) => {
   try {
     const response = await fetch(`https://api.chucknorris.io/jokes/random`);
     const data = await response.json();
 
+    if (contanisBadWord(data.value, profanity)) {
+      return fetchChuckNorrisJoke(profanity);
+    }
+    
     return data.value;
   } catch (error) {
     console.error("Error fetching Chuck Norris joke:", error);
   }
 };
 
+function contanisBadWord(string, list) {
+  for (const word of list) {
+    if (string.toLowerCase().includes(word.toLowerCase())) {
+      return true
+    }
+  }
+  
+  return false; 
+}
+
 export const Chuckles = async (questionsToAsk) => {
   let jokes = [];
 
   for (let i = 0; i < questionsToAsk; i++) {
-    jokes.push(await fetchChuckNorrisJoke());
+    jokes.push(await fetchChuckNorrisJoke(profanity));
   }
 
   return jokes;
